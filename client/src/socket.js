02ec063cae118227c721a157f0de8fd9014482ab
Qluant <kovalenko.a.2006@gmail.com>
2026-05-08 17:25:38 +0300

    fix: cleaning client folder

--- client/src/socket.js
+++
@@ -1,8 +0,0 @@
-import { io } from 'socket.io-client';
-
-// Connect to backend WebSocket server
-const SOCKET_URL = 'http://localhost:5000';
-
-export const socket = io(SOCKET_URL, {
-  autoConnect: false, // We'll connect manually when needed
-});
